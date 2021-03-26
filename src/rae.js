0b7d3ace123e2b4801bc102f3152f5f00875b683
InSight <cvveglia@hotmail.com>
2021-03-26 16:04:59 -0300

    push proyect

--- src/rae.js
+++ src/rae.js
@@ -4,10 +4,6 @@ const config = require("../config");
 
 const dsClient = new Discord.Client();
 
-dsClient.on("ready", () => {
-	console.log("ready");
-});
-
 dsClient.on("message", async (msg) => {
 	if (msg.channel.name === "rae" && msg.content.includes(config.prefix)) {
 		const word = msg.content.split("!rae ")[1];
