221df6efb4c39364de404e7d86972dd8156dc8bf
InSight <cvveglia@hotmail.com>
2021-03-26 15:32:10 -0300

    push proyect

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	token: "ODI0NzE2OTk3NDQyNjY2NTY2.YFzbjg.YODHZX3wtNUwIRiCBZMJU2yP8GM",
+	token: "ODI0NzE2OTk3NDQyNjY2NTY2.YFzbjg.qK6HgN0nCrLxmBXi0DexzqluSfU",
 	prefix: "!rae",
 };
