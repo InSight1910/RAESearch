6e17b77e34622f292bfe1c3dd9ac662a63bf55d3
Vicente Espinosa <61396211+InSight1910@users.noreply.github.com>
2021-03-26 16:36:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	token: "ODI0NzE2OTk3NDQyNjY2NTY2.YFzbjg.qK6HgN0nCrLxmBXi0DexzqluSfU",
+	token: "ODI0NzE2OTk3NDQyNjY2NTY2.YFzbjg._og0Aux_2C6dtBD7g7kIrYf-baQ",
 	prefix: "!rae",
 };
